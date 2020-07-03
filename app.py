4ba079da54574eb33857ecc785f5615befe84eaf
camilo <rodriguez.camilo@gmail.com>
2020-07-03 16:59:11 -0500

    a flask app with azure bus communication for aks

---
+++ app.py
@@ -0,0 +1,27 @@
+from azure.servicebus import QueueClient, Message
+CONNECTION = 'Endpoint=sb://queue255423.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=sLWuWACutrI7iqcAyhnFoG8sk/YQ+QhwwuAeV9bfsHE='
+# Create the QueueClient
+queue_client = QueueClient.from_connection_string(CONNECTION, "taskqueue")
+
+# Send a test message to the queue
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Index Page'
+
+@app.route('/hello')
+def hello():
+  return 'Hello, greetings from different endpoint'
+
+#adding variables
+@app.route('/send/<msg>')
+def show_user(msg):
+  message = Message(msg)
+  queue_client.send(message)	
+  return 'Message Sent: %s' % message
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
